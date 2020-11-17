cebabe40eec4e98708037d9cc42a07dfa6d922a2
Eugene Theriault <hybridbassist@gmail.com>
2020-11-16 20:13:00 -0800

    Update file structure

---
+++ lib/asteroid.rb
@@ -0,0 +1,11 @@
+class Asteroid
+  attr_reader :name,
+              :miss_distance,
+              :estimated_diameter
+
+  def initialize(response_data)
+    @name = response_data[:name]
+    @miss_distance = response_data[:close_approach_data][0][:miss_distance][:miles]
+    @estimated_diameter = response_data[:estimated_diameter][:feet][:estimated_diameter_max]
+  end
+end
