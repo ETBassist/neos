c13341c1482719169a2446d71da98dc6471dabe1
Eugene Theriault <hybridbassist@gmail.com>
2020-11-19 10:37:45 -0800

    Some changes for readability

--- lib/neo_service.rb
+++ lib/neo_service.rb
@@ -1,6 +1,5 @@
 require 'faraday'
 require 'figaro'
-require 'pry'
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../../config/application.yml', __FILE__))
 Figaro.load
@@ -11,11 +10,9 @@ class NEOService
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
-    asteroids_list_data = connection.get('/neo/rest/v1/feed')
+    asteroids_list_response = connection.get('/neo/rest/v1/feed')
+    parsed_asteroids_data = JSON.parse(asteroids_list_response.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-    parsed_asteroids_data.map do |asteroid_data|
-      Asteroid.new(asteroid_data)
-    end
+    parsed_asteroids_data.map { |asteroid_data| Asteroid.new(asteroid_data) }
   end
 end
